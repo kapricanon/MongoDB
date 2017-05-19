8684c99dca1f39902da5053d64883669fa4a937f
kapricanon <kapricanon@yahoo.com>
2017-05-19 16:41:58 +0100

    minor tweak

--- src/main/java/com/example/demo/DomainRepositoryImpl.java
+++ src/main/java/com/example/demo/DomainRepositoryImpl.java
@@ -25,15 +25,15 @@ public class DomainRepositoryImpl implements DomainRepositoryCustom {
       Update update = new Update();
       update.set("displayAds", displayAds);
 
-      //Domain testDomain = new Domain();
-      //testDomain.setDomain("test1");
-      //testDomain.setId(1);
-     // mongoTemplate.insert(testDomain);
-     // DBCollection collection = mongoTemplate.getCollection("domain");
-      //DBCursor cur = collection.find();
-      //for(DBObject c : cur) {
-    //	  System.out.println(c);
-     // }
+      /*Domain testDomain = new Domain();
+      testDomain.setDomain("test1");
+      testDomain.setId(1);
+      mongoTemplate.insert(testDomain);
+      DBCollection collection = mongoTemplate.getCollection("domain");
+      DBCursor cur = collection.find();
+      for(DBObject c : cur) {
+    	  System.out.println(c);
+      }*/
     
       WriteResult result = mongoTemplate.updateFirst(query, update, Domain.class);
 
